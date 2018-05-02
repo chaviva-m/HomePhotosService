4c645caff78548475c86ae49c4aa0fee09971632
leah <leahorlin@gmail.com>
2018-05-02 17:53:30 +0300

    log view

--- GUI/View/LogView.xaml.cs
+++ GUI/View/LogView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,12 @@ namespace GUI.View
     /// </summary>
     public partial class LogView : Window
     {
+        private LogViewModel vm();
         public LogView()
         {
             InitializeComponent();
+            vm = new LogViewModel();
+            this.DataContext = vm;
         }
     }
 }
